caad1f96b75d1326a994922db601e3a491937187
Mateus Nadal <nlmatt07@hotmail.com>
2020-07-17 21:51:18 -0300

    Componente

--- conceitos-react-js/src/App.js
+++ conceitos-react-js/src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header'
 
 function App() {
+
+    const { projects, serProjects } = useState(['Desenvolvimento de App', 'Web']);
+
+
+    function handleAddProject() {
+        serProjects([ ...projects,`Novo Projeto ${Date.now()}` ]);
+    }
+
     return (
-    <>
-    <Header/>
-    <Header/>
-    </>
+        <>
+            <Header title="Projects" />
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+
+        </>
 
     );
 }
-
 export default App;
